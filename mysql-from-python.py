223721667c516cc52a1232f5a8a702c148a44994
Aiden Harvey <aidenharvey102@gmail.com>
2019-02-08 16:05:12 +0000

    alternate delete with string interpolation

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -15,7 +15,7 @@ connection = pymysql.connect(host='localhost',
 try:
     # run a query on database
     with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        rows = cursor.execute("DELETE FROM Friends WHERE name = 'Bob';")
+        rows = cursor.execute("DELETE fROM Friends WHERE name = %s", 'Bob')
         connection.commit()
         
 finally:
